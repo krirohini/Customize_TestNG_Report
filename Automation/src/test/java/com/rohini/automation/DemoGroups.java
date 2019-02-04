83a15adbaa17d7a7aebef02db5b4613df84fd473
Rohini Kumari <krirohini@gmail.com>
2019-02-04 10:25:22 -0800

    first commit

--- Automation/src/test/java/com/rohini/automation/DemoGroups.java
+++ Automation/src/test/java/com/rohini/automation/DemoGroups.java
@@ -1,9 +1,24 @@
 package com.rohini.automation;
 
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.awt.AWTException;
+import java.awt.FlowLayout;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+ 
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 
 public class DemoGroups {
 
@@ -44,6 +59,49 @@ public class DemoGroups {
 	//@Test(priority = 0)
 	public void testCase_3() {
 		System.out.println(">>>> " + new Throwable().getStackTrace()[0].getMethodName());
+	}
+	
+	@Test(priority = 0, groups = {"sanity_if_time"})
+	//@Test(priority = 0)
+	public void testCase_4() {
+		Assert.assertEquals("rk", "blk");
+		System.out.println(">>>> " + new Throwable().getStackTrace()[0].getMethodName());
+	}
+	
+	@Test(groups = {"sanity_if_time"}, enabled=false)
+	//@Test(priority = 0)
+	public void testCase_5() throws InterruptedException {
+		Thread.sleep(2000);
+		Assert.assertEquals("rk", "blk");
+		System.out.println(">>>> " + new Throwable().getStackTrace()[0].getMethodName());
+	}
+	
+	public void takeScreenshot() {
+		
+	      try {
+	        /*
+	         * Let the program wait for 5 seconds to allow you to open the
+	         * window whose screenshot has to be captured
+	         */
+	         Thread.sleep(5000);
+	         Robot robot = new Robot();
+	         String fileName = "./FullScreenshot.jpg";
+	 
+	         Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit()
+	                                     .getScreenSize());
+	         BufferedImage screenFullImage = robot.createScreenCapture(screenRect);
+	         ImageIO.write(screenFullImage, "jpg", new File(fileName));
+	 
+	         /*f.setLocation(500, 500);
+	         JLabel text = new JLabel("A full screenshot saved!");
+	         f.add(text);
+	         f.setSize(200, 100);
+	         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	         f.getContentPane().setLayout(new FlowLayout());
+	         f.setVisible(true);*/
+	      } catch (AWTException | IOException | InterruptedException ex) {
+	               System.err.println(ex);
+	      }
 	}
 	
 }
